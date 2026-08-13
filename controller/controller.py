e3793b6eada5ed8d75bf3267ba804bc9e19a6ad1
Qurrath-ul-ain <syedaqurrathulain210@gmail.com>
2026-08-13 13:27:55 +0530

    Update controller.py

--- controller/controller.py
+++ controller/controller.py
@@ -1,1 +1,111 @@
+#!/usr/bin/env python3
+"""
+controller.py
+Stage 3 - Basic Ryu controller for the healthcare SDN-IDS project.
 
+This is a standard L2 learning switch built on Ryu's OpenFlow 1.3 API.
+It lets traffic flow between hosts (h1-h4) in the Mininet topology.
+
+Later stages will extend this file to:
+  - Stage 11: periodically poll and export flow/port statistics
+              (for the detection module to consume)
+  - Stage 12: accept mitigation commands (block/isolate a host's flows)
+
+Run with (from the controller/ directory):
+    ryu-manager controller.py
+
+Then, in a separate terminal, start the Mininet topology:
+    sudo python3 ../mininet/topology.py
+
+Verify:
+    Inside the mininet> CLI, run `pingall` - all 4 hosts should reach
+    each other with 0% dropped once this controller is running.
+"""
+
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet, ethernet, ether_types
+
+
+class HealthcareSwitch(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(HealthcareSwitch, self).__init__(*args, **kwargs)
+        # mac_to_port[dpid][mac_address] = port_no
+        self.mac_to_port = {}
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        """Install a default 'send everything to controller' rule
+        when a switch first connects."""
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                           ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+        self.logger.info("Switch connected: dpid=%s", datapath.id)
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                     priority=priority, match=match,
+                                     instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                     match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        dpid = datapath.id
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            return  # ignore LLDP
+
+        dst = eth.dst
+        src = eth.src
+
+        self.mac_to_port.setdefault(dpid, {})
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                   in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
